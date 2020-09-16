a4d3365504087a956565912e7e90ff1c5d6144bf
fabio lopes <fabiolopes7844@gmail.com>
2020-09-15 23:08:58 -0300

    Added DbSet User

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -7,7 +7,7 @@ namespace DatingApp.API.Data
     {
 
         public DataContext(DbContextOptions<DataContext> options) : base(options){}        
-
         public DbSet<Value> Values { get; set;}
+        public DbSet<User> Users {get; set;}
     }
 }
