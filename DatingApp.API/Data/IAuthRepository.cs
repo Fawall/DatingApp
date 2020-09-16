fa62ad5bfb08203add346b4a5f1870188f34d149
fabio lopes <fabiolopes7844@gmail.com>
2020-09-15 23:06:05 -0300

    Creating AuthRepository and Register method and Login Method

---
+++ DatingApp.API/Data/IAuthRepository.cs
@@ -0,0 +1,14 @@
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public interface IAuthRepository
+    {
+        Task<User> Register(User user, string password);
+        Task<User> Login(string username, string password);
+        Task<bool> UserExists(string username);
+
+
+    }
+}
