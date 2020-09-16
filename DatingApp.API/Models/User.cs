21be6e03ca1fcc84054c618cbe30caae511d3198
fabio lopes <fabiolopes7844@gmail.com>
2020-09-15 23:00:33 -0300

    Creating User Model

---
+++ DatingApp.API/Models/User.cs
@@ -0,0 +1,11 @@
+namespace DatingApp.API.Models
+{
+    public class User
+    {
+        public int Id {get; set;}
+        public string Username {get; set;}
+        public byte[] PasswordHash {get; set;}
+        public byte[] PasswordSalt {get; set;}
+
+    }
+}
