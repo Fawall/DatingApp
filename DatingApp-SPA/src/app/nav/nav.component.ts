ad30e450cb83418b130bc0c3b4105e8ef8611db9
fabio lopes <fabiolopes7844@gmail.com>
2020-09-29 13:00:16 -0300

    Added NavComponent

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {}
+
+  constructor(private authService : AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login(){
+    this.authService.login(this.model).subscribe(next =>{
+      console.log("Logged is Succefully")
+    }, error => {
+      console.log("Failed is login");
+    });
+  }
+
+}
