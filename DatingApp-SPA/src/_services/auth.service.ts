8e0f092394663b762aadcb244932aaf5760a00ce
fabio lopes <fabiolopes7844@gmail.com>
2020-10-07 11:57:10 -0300

    Added methods login and register

--- DatingApp-SPA/src/_services/auth.service.ts
+++ DatingApp-SPA/src/_services/auth.service.ts
@@ -11,16 +11,19 @@ export class AuthService {
 
 constructor(private http: HttpClient) { }
 
-login(model: any){
-  return this.http.post(this.baseUrl + "login", model).pipe(
-    map((response: any) => {
-      const user = response;
-      if(user){
-        localStorage.setItem("token", user.token)
-      }
-    })
-  )
-}
+  login(model: any){
+    return this.http.post(this.baseUrl + "login", model).pipe(
+      map((response: any) => {
+        const user = response;
+        if(user){
+          localStorage.setItem("token", user.token)
+        }
+      })
+    )
+  }
 
+  register(model: any){
+    return this.http.post(this.baseUrl + 'register', model);
+  }
 
 }
